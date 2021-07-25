1d9f5082da86c0f52a22f464d855000601bcfdf3
KobiKisos <87780800+KobiKisos@users.noreply.github.com>
2021-07-25 07:33:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,248 @@
+/*----- constants -----*/
+
+// creating constants
+var bombImage = '<img src="images/bomb.png">';
+var flagImage = '<img src="images/flag.png">';
+var wrongBombImage = '<img src="images/wrong-bomb.png">'
+var sizeLookup = {
+  '4': {totalBombs: 2, tableWidth: '245px'},  //level1
+  '8': {totalBombs: 12, tableWidth: '420px'}, //level3
+  '16': {totalBombs: 30, tableWidth: '600px'} //level3
+};
+var colors = [
+  '',
+  '#0000FA',
+  '#4B802D',
+  '#DB1300',
+  '#202081',
+  '#690400',
+  '#457A7A',
+  '#1B1B1B',
+  '#7A7A7A',
+];
+
+/*----- app's state (variables) -----*/
+var size = 16;
+var board;
+var bombCount;
+var timeElapsed;
+var adjBombs;
+var hitBomb;
+var elapsedTime;
+var timerId;
+var winner;
+
+/*----- cached element references -----*/
+var boardEl = document.getElementById('board');
+
+/*----- event listeners -----*/
+document.getElementById('size-btns').addEventListener('click', function(e) {
+  size = parseInt(e.target.id.replace('size-', ''));
+  initGame();
+  renderBoard();
+});
+
+boardEl.addEventListener('click', function(e) {
+  if (winner || hitBomb) return;
+  var clickedEl;
+  clickedEl = e.target.tagName.toLowerCase() === 'img' ? e.target.parentElement : e.target;
+  if (clickedEl.classList.contains('game-cell')) {
+    if (!timerId) setTimer();
+    var row = parseInt(clickedEl.dataset.row);
+    var col = parseInt(clickedEl.dataset.col);
+    var cell = board[row][col];
+    if (e.shiftKey && !cell.revealed && bombCount > 0) {
+      bombCount += cell.flag() ? -1 : 1;
+    } else {
+      hitBomb = cell.reveal();
+      if (hitBomb) {
+        revealAll();
+        clearInterval(timerId);
+        e.target.style.backgroundColor = 'red';
+      }
+    }
+    winner = getWinner();
+    renderBoard();
+  }
+});
+
+function createResetListener() { 
+  document.getElementById('reset').addEventListener('click', function() {
+    initGame();
+    renderBoard();
+  });
+}
+
+/*----- functions -----*/
+function setTimer () {
+  timerId = setInterval(function(){
+    elapsedTime += 1;
+    document.getElementById('timer').innerText = elapsedTime.toString().padStart(3, '0');
+  }, 1000);
+};
+
+//execute at end of game
+function revealAll() {
+  board.forEach(function(rowArr) {
+    rowArr.forEach(function(cell) {
+      cell.reveal();
+    });
+  });
+};
+
+// parses the number from the ID on the table 
+// which converts it into a ${size} variable which basically appends rows/cols based on the number it grabs
+function buildBoard() {
+  var topRow = `
+  <tr>
+    <td class="menu" id="window-title-bar" colspan="${size}">
+      <div id="window-title"><img src="images/mine-menu-icon.png"> Kobi's Minesweeper</div>
+    </td>
+  <tr>
+    </td>
+  </tr>
+  </tr>
+    <tr>
+      <td class="menu" colspan="${size}">
+          <section id="status-bar">
+            <div id="bomb-counter">000</div>
+            <div id="reset"><img src="IMG/letsPlayPepe.png" class="center"></div>
+            <div id="timer">000</div>
+          </section>
+      </td>
+    </tr>
+    `;
+  boardEl.innerHTML = topRow + `<tr>${'<td class="game-cell"></td>'.repeat(size)}</tr>`.repeat(size);
+  boardEl.style.width = sizeLookup[size].tableWidth;
+  createResetListener();
+  var cells = Array.from(document.querySelectorAll('td:not(.menu)'));
+  cells.forEach(function(cell, idx) {
+    cell.setAttribute('data-row', Math.floor(idx / size));
+    cell.setAttribute('data-col', idx % size);
+  });
+}
+
+function buildArrays() {
+  var arr = Array(size).fill(null);
+  arr = arr.map(function() {
+    return new Array(size).fill(null);
+  });
+  return arr;
+};
+
+function buildCells(){
+  board.forEach(function(rowArr, rowIdx) {
+    rowArr.forEach(function(slot, colIdx) {
+      board[rowIdx][colIdx] = new Cell(rowIdx, colIdx, board);
+    });
+  });
+  addBombs();
+  runCodeForAllCells(function(cell){
+    cell.calcAdjBombs();
+  });
+};
+
+function initGame() {
+  buildBoard();
+  board = buildArrays();
+  buildCells();
+  bombCount = getBombCount();
+  elapsedTime = 0;
+  clearInterval(timerId);
+  timerId = null;
+  hitBomb = false;
+  winner = false;
+};
+
+function getBombCount() {
+  var count = 0;
+  board.forEach(function(row){
+    count += row.filter(function(cell) {
+      return cell.bomb;
+    }).length
+  });
+  return count;
+};
+
+function addBombs() {
+  var currentTotalBombs = sizeLookup[`${size}`].totalBombs;
+  while (currentTotalBombs !== 0) {
+    var row = Math.floor(Math.random() * size);
+    var col = Math.floor(Math.random() * size);
+    var currentCell = board[row][col]
+    if (!currentCell.bomb){
+      currentCell.bomb = true
+      currentTotalBombs -= 1
+    }
+  }
+};
+
+function getWinner() {
+  for (var row = 0; row<board.length; row++) {
+    for (var col = 0; col<board[0].length; col++) {
+      var cell = board[row][col];
+      if (!cell.revealed && !cell.bomb) return false;
+    }
+  } 
+  return true;
+};
+
+function renderBoard() {
+  document.getElementById('bomb-counter').innerText = bombCount.toString().padStart(3, '0');
+  var seconds = timeElapsed % 60;
+  var tdList = Array.from(document.querySelectorAll('[data-row]'));
+  tdList.forEach(function(td) {
+    var rowIdx = parseInt(td.getAttribute('data-row'));
+    var colIdx = parseInt(td.getAttribute('data-col'));
+    var cell = board[rowIdx][colIdx];
+    if (cell.flagged) {
+      td.innerHTML = flagImage;
+    } else if (cell.revealed) {
+      if (cell.bomb) {
+        td.innerHTML = bombImage;
+      } else if (cell.adjBombs) {
+        td.className = 'revealed'
+        td.style.color = colors[cell.adjBombs];
+        td.textContent = cell.adjBombs;
+      } else {
+        td.className = 'revealed'
+      }
+    } else {
+      td.innerHTML = '';
+    }
+  });
+  if (hitBomb) {
+    document.getElementById('reset').innerHTML = '<img src=IMG/losingPepe.png class = center>';
+    alert('YOU LOST! click on Pepe to reset Board');
+    runCodeForAllCells(function(cell) {
+      if (!cell.bomb && cell.flagged) {
+        var td = document.querySelector(`[data-row="${cell.row}"][data-col="${cell.col}"]`);
+        td.innerHTML = wrongBombImage;
+      }
+    });
+  } else if (winner) {
+    document.getElementById('reset').innerHTML = '<img src=IMG/WinnergPepe.png class = center>';
+    $('document').ready(function(){
+      $gif = $('img').first();
+      alert('YOU WON!!! click on Pepe to reset Board');
+      $('#gif-trigger').on('click', function(){
+        src = $gif.attr('src');
+        
+        $gif.attr('src', src);
+      });
+    });
+
+    clearInterval(timerId);
+  }
+};
+
+function runCodeForAllCells(cb) {
+  board.forEach(function(rowArr) {
+    rowArr.forEach(function(cell) {
+      cb(cell);
+    });
+  });
+}
+
+initGame();
+renderBoard();
---
+++ readme.md
@@ -0,0 +1,4 @@
+MineSweeper Project for Coding Academy 
+by : Kobi Kisos
+
+project is written in HTML,JS and CSS.
